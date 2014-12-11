a18662fd98a7a5d05e7e2219f4f8f6cf7c0b80ca
t1rex <newt1rex@gmail.com>
2014-12-11 15:27:31 +0200

    first commit

---
+++ db/connect_to_db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nazar
+ * Date: 11.12.14
+ * Time: 15:17
+ */ 
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nazar
+ * Date: 11.12.14
+ * Time: 10:56
+ */
+
+//$xx = 1 + 34;
+//echo $xx;
+//
+//$xx += 1;
+//echo $xx;
+//
+//$xx += 1;
+//echo $xx;
+//
+//echo  phpversion() ;
+//
+//echo 'rasdkjhf';
+//
+//phpinfo(INFO_MODULES);
+
+include 'html/first.html';
